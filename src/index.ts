530b11f37023660f50f70fda17b89c67aaf15095
Rui Gomes <hello@ruigomes.me>
2021-02-20 12:04:48 +0000

    Format code to new prettier rules

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,28 @@ import { Buffer } from 'buffer';
 
 const base58 = baseX('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz');
 
-const sha256 = payload => Buffer.from(sha('sha256').update(payload).digest());
+const sha256 = (payload) => Buffer.from(sha('sha256').update(payload).digest());
 
 const addressTypes = {
   0x00: {
     type: 'p2pkh',
-    network: 'mainnet'
+    network: 'mainnet',
   },
 
   0x6f: {
     type: 'p2pkh',
-    network: 'testnet'
+    network: 'testnet',
   },
 
   0x05: {
     type: 'p2sh',
-    network: 'mainnet'
+    network: 'mainnet',
   },
 
   0xc4: {
     type: 'p2sh',
-    network: 'testnet'
-  }
+    network: 'testnet',
+  },
 };
 
 const validateBech32 = (address) => {
@@ -41,8 +41,8 @@ const validateBech32 = (address) => {
   const prefixesNetwork = {
     bc: 'mainnet',
     tb: 'testnet',
-    bcrt: 'regtest'
-  }
+    bcrt: 'regtest',
+  };
 
   const network = prefixesNetwork[decoded.prefix];
 
@@ -70,7 +70,7 @@ const validateBech32 = (address) => {
     bech32: true,
     network,
     address,
-    type
+    type,
   };
 };
 
@@ -109,9 +109,7 @@ const validateBtcAddress = (address) => {
     return false;
   }
 
-  return addressTypes[version]
-    ? Object.assign({ address, bech32: false }, addressTypes[version])
-    : false;
+  return addressTypes[version] ? Object.assign({ address, bech32: false }, addressTypes[version]) : false;
 };
 
 const strictValidation = (address, network) => {
